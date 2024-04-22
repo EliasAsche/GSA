ddaf5028e1a6d9815bb4889d17e39ba6f231457e
Elias Aschenbrenner <139927683+EliasAsche@users.noreply.github.com>
2024-04-22 08:01:55 -0400

    changing main to run on awhile loop so the user may continously run application

--- main.cpp
+++ main.cpp
@@ -4,49 +4,44 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <chrono>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cout << "Usage: " << argv[0] << " <file_path> <sort_field> <sort_method>" << endl;
-        cout << "sort_field: age or amount" << endl;
-        cout << "sort_method: merge or quick" << endl;
-        return 1;
-    }
 
-    string filePath = argv[1];
-    string sortField = argv[2];
-    string sortMethod = argv[3];
 
     Transactions transactions;
     transactions.readTransactionsCaller(filePath);
 
-    vector<Transactions::Node*> sortedNodes;
+    vector<Transactions::Node*> sortedNodesMerge;
+    vector<Transactions::Node*> sortedNodesQuick;
 
     if (sortField == "age") {
-        if (sortMethod == "merge") {
-            sortedNodes = transactions.mergeSortAge(transactions.getNodes());
-        } else if (sortMethod == "quick") {
-            sortedNodes = transactions.quickSortAge(transactions.getNodes());
-        } else {
-            cout << "Invalid sort method. Use 'merge' or 'quick'." << endl;
-            return 1;
-        }
-    } else if (sortField == "amount") {
-        if (sortMethod == "merge") {
-            sortedNodes = transactions.mergeSortAmount(transactions.getNodes());
-        } else if (sortMethod == "quick") {
-            sortedNodes = transactions.quickSortAmount(transactions.getNodes());
-        } else {
-            cout << "Invalid sort method. Use 'merge' or 'quick'." << endl;
-            return 1;
-        }
-    } else {
-        cout << "Invalid sort field. Use 'age' or 'amount'." << endl;
-        return 1;
+        auto beginMerge = chrono::high_resolution_clock::now();
+        sortedNodesMerge = transactions.mergeSortAge(transactions.getNodes());
+        auto endMerge = chrono::high_resolution_clock::now();
+        auto durationMerge = chrono::duration_cast<std::chrono::milliseconds>(endMerge - beginMerge);
+        auto beginQuick = chrono::high_resolution_clock::now();
+        sortedNodesQuick = transactions.quickSortAge(transactions.getNodes());
+        auto endQuick = chrono::high_resolution_clock::now();
+        auto durationQuick = chrono::duration_cast<std::chrono::milliseconds>(endQuick - beginQuick);
+    }
+    else if (sortField == "amount") {
+        auto beginMerge = chrono::high_resolution_clock::now();
+        sortedNodesMerge = transactions.mergeSortAmount(transactions.getNodes());
+        auto endMerge = chrono::high_resolution_clock::now();
+        auto durationMerge = chrono::duration_cast<std::chrono::milliseconds>(endMerge - beginMerge);
+        sortedNodesQuick = transactions.quickSortAmount(transactions.getNodes());
     }
 
-    for (const auto& node : sortedNodes) {
+    for (const auto& node : sortedNodesMerge) {
+        cout << "Transaction Type: " << node->transactionType
+             << ", Amount: $" << node->transactionAmount
+             << ", Account Age: " << node->accountAge
+             << ", Is Fraudulent: " << (node->isFraudulent ? "Yes" : "No") << endl;
+    }
+
+    for (const auto& node : sortedNodesQuick) {
         cout << "Transaction Type: " << node->transactionType
              << ", Amount: $" << node->transactionAmount
              << ", Account Age: " << node->accountAge
