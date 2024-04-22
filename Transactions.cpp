173699250b53c794334134be5179e4869e20f1bd
Elias Aschenbrenner <139927683+EliasAsche@users.noreply.github.com>
2024-04-21 22:09:55 -0400

    adding new classes to contain our data along with the sorting algorthim

---
+++ Transactions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elias on 4/21/2024.
+//
+
+#include "Transactions.h"
---
+++ Transactions.h
@@ -0,0 +1,14 @@
+//
+// Created by elias on 4/21/2024.
+//
+
+#ifndef GSA_TRANSACTIONS_H
+#define GSA_TRANSACTIONS_H
+
+
+class Transactions {
+
+};
+
+
+#endif //GSA_TRANSACTIONS_H
