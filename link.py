d4f01a1aba8d3b5aaaf644653f01b061da8c9ccf
myakhatri <127243859+myakhatri@users.noreply.github.com>
2024-04-22 23:28:46 -0400

    Update link.py

--- link.py
+++ link.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    # loading the csv from the path
+    #loading in the csv file from the path
     df = pd.read_csv(r"C:\Users\myakh\PycharmProjects\DSA Project 3\Fraudulent_E-Commerce_Transaction_Data.csv")
 
-    # selecting the columns by index
-    # one less than the actual column number
+    #selecting the columns by index
+    #they are indexed as one less than the actual column number
     selected_columns = df.iloc[:, [2, 4, 13, 14]]
 
-    # Display the isolated columns
+    #displaying the isolated/separated columns
     print(selected_columns)
 
-    # write the columns to a new file
+    #writing the columns to a new txt file
     selected_columns.to_csv(r"C:\Users\myakh\PycharmProjects\DSA Project 3\new commerce data.txt", index=False)
