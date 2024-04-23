8e46de7f100093d114e167ce7dd006ba298c761a
Elias Aschenbrenner <139927683+EliasAsche@users.noreply.github.com>
2024-04-22 23:32:02 -0400

    Update Transactions.cpp
    extended commenting to explain functions

--- Transactions.cpp
+++ Transactions.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-vector<Transactions::Node*> mergeByAge(const vector<Transactions::Node*>& left, const vector<Transactions::Node*>& right) { // helper funjcti0 
+vector<Transactions::Node*> mergeByAge(const vector<Transactions::Node*>& left, const vector<Transactions::Node*>& right) { // helper funjction called by merge sort to actually merge the two vectors together
     vector<Transactions::Node*> result;
     unsigned leftIndex = 0, rightIndex = 0;
     while (leftIndex < left.size() && rightIndex < right.size()) {
@@ -26,10 +26,10 @@ vector<Transactions::Node*> Transactions::mergeSortAge(const vector<Node*>& node
     if (nodes.size() <= 1) {
         return nodes;
     }
-    int mid = nodes.size() / 2;
+    int mid = nodes.size() / 2; // finding the midpoint for the sort
     vector<Node*> left(nodes.begin(), nodes.begin() + mid);
     vector<Node*> right(nodes.begin() + mid, nodes.end());
-    return mergeByAge(mergeSortAge(left), mergeSortAge(right));
+    return mergeByAge(mergeSortAge(left), mergeSortAge(right)); // recursive call of merge sort and actually merge
 }
 
 vector<Transactions::Node*> Transactions::quickSortAge(const vector<Node*>& nodes) const {
@@ -38,7 +38,7 @@ vector<Transactions::Node*> Transactions::quickSortAge(const vector<Node*>& node
     }
     vector<Node*> less, equal, greater;
     Node* pivot = nodes[nodes.size() / 2];
-    for (Node* node : nodes) {
+    for (Node* node : nodes) { // distribution doen here based on comparison with the pivot
         if (node->accountAge < pivot->accountAge) {
             less.push_back(node);
         } else if (node->accountAge > pivot->accountAge) {
@@ -47,14 +47,14 @@ vector<Transactions::Node*> Transactions::quickSortAge(const vector<Node*>& node
             equal.push_back(node);
         }
     }
-    less = quickSortAge(less);
+    less = quickSortAge(less); // begin recursive calling of non equal groups
     greater = quickSortAge(greater);
     less.insert(less.end(), equal.begin(), equal.end());
-    less.insert(less.end(), greater.begin(), greater.end());
+    less.insert(less.end(), greater.begin(), greater.end()); // insert the sorted groups
     return less;
 }
 
-vector<Transactions::Node*> mergeByAmount(const vector<Transactions::Node*>& left, const vector<Transactions::Node*>& right) {
+vector<Transactions::Node*> mergeByAmount(const vector<Transactions::Node*>& left, const vector<Transactions::Node*>& right) { // same as other merge but with amount as the parameter this time
     vector<Transactions::Node*> result;
     unsigned leftIndex = 0, rightIndex = 0;
     while (leftIndex < left.size() && rightIndex < right.size()) {
@@ -72,7 +72,7 @@ vector<Transactions::Node*> mergeByAmount(const vector<Transactions::Node*>& lef
 }
 
 
-vector<Transactions::Node*> Transactions::mergeSortAmount(const vector<Node*>& nodes) const {
+vector<Transactions::Node*> Transactions::mergeSortAmount(const vector<Node*>& nodes) const { // Same as other merge but with amount as the parameter this time
     if (nodes.size() <= 1) {
         return nodes;
     }
@@ -82,7 +82,7 @@ vector<Transactions::Node*> Transactions::mergeSortAmount(const vector<Node*>& n
     return mergeByAmount(mergeSortAmount(left), mergeSortAmount(right));
 }
 
-vector<Transactions::Node*> Transactions::quickSortAmount(const vector<Node*>& nodes) const {
+vector<Transactions::Node*> Transactions::quickSortAmount(const vector<Node*>& nodes) const { // same as other quick sort but with amount as the new parameter
     if (nodes.size() <= 1) {
         return nodes;
     }
@@ -105,7 +105,7 @@ vector<Transactions::Node*> Transactions::quickSortAmount(const vector<Node*>& n
 }
 
 
-void Transactions::readTransactions(const string& filename) {
+void Transactions::readTransactions(const string& filename) { // this is a function to read in the values of transactions from a txt
     ifstream file(filename);
     string line;
     getline(file, line);
@@ -124,21 +124,21 @@ void Transactions::readTransactions(const string& filename) {
         bool isFraudulent = transactionData[2] == "1";
         int age = stoi(transactionData[3]);
 
-        Node* newNode = new Node(type, amount, age, isFraudulent);
-        transactionNodes.push_back(newNode);
+        Node* newNode = new Node(type, amount, age, isFraudulent); // create new nodes from the txt and call the constructor to create them with specified attributes
+        transactionNodes.push_back(newNode); // push them into transactions vector
     }
     file.close();
 }
 
-void Transactions::readTransactionsCaller(const std::string &filename) {
+void Transactions::readTransactionsCaller(const std::string &filename) { //caller so user can't directly interact with data
     readTransactions(filename);
 }
 
-vector<Transactions::Node*> Transactions::getNodes() const{
+vector<Transactions::Node*> Transactions::getNodes() const{ //helper functions
     return this->transactionNodes;
 }
 
-double Transactions::getAvgAccountAge(const std::vector<Node*>& nodes) const {
+double Transactions::getAvgAccountAge(const std::vector<Node*>& nodes) const { // function that iterates through all nodes to find the average account age of fraud users
     double totalAge = 0;
     int count = 0;
     for (const Node* node : nodes) {
@@ -154,7 +154,7 @@ double Transactions::getAvgAccountAge(const std::vector<Node*>& nodes) const {
     }
 }
 
-double Transactions::getAvgTransactionAmount(const std::vector<Node*>& nodes) const {
+double Transactions::getAvgTransactionAmount(const std::vector<Node*>& nodes) const { // iterates to find avg transaciotn amount of fraud transacations
     double totalAmount = 0;
     int count = 0;
     for (const Node* node : nodes) {
@@ -171,7 +171,7 @@ double Transactions::getAvgTransactionAmount(const std::vector<Node*>& nodes) co
     }
 }
 
-void Transactions::getFraudulentRate(const std::vector<Node*>& nodes) const {
+void Transactions::getFraudulentRate(const std::vector<Node*>& nodes) const { // this functions uses counters and iterates through the vector of nodes to find the average faud rate of each tranascion type
     double btCount = 0;
     double creditCount = 0;
     double debitCount = 0;
